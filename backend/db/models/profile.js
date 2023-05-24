bcb9119c3d61f10caca35e6cff0d7bbe65326313
MichalScigalski <michalscigalski0@gmail.com>
2023-05-24 19:47:03 +0200

    added schema to have id for each link

--- backend/db/models/profile.js
+++ backend/db/models/profile.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose')
 
+const LinkSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+})
+
 const ProfileSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
     },
-    links: []
+    links: [LinkSchema]
 })
 
 const Profile = mongoose.model('Profile', ProfileSchema)
