6c49d915c22d61c2ba8a990ab36f2377c87740e8
MichalScigalski <michalscigalski0@gmail.com>
2023-06-03 20:30:48 +0200

    register component functionality implement

--- client/src/routes/Register/Register.component.js
+++ client/src/routes/Register/Register.component.js
@@ -1,16 +1,60 @@
+import axios from 'axios'
 import { RegisterContainer, FormContainer } from './Register.style'
 import RegisterPicture from '../../assets/img/login-picture.svg'
 import FormField from '../../components/FormField/FormField.component'
 import Button from '../../components/Button/Button.component'
+import { useState } from 'react'
 
 const Register = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordRepeat, setPasswordRepeat] = useState('')
+
+    const RegisterHandler = async (e) => {
+        e.preventDefault()
+        if(password !== passwordRepeat)
+            return alert('Passwords are different!')
+        try {
+            const res = await axios.post('http://localhost:3002/api/v1/user/register', {
+                username, 
+                password
+            })
+            if(res.data) {
+                alert('Register success')
+            }
+        } catch (err) {
+            alert(err.response.data.message)
+            setPassword('')
+            setPasswordRepeat('')
+        }
+    }
+
     return (
         <RegisterContainer>
             <FormContainer>
-                <form>
-                    <FormField label={'Username'} placeholder={'username'} />
-                    <FormField label={'Password'} placeholder={'**********'} />
-                    <FormField label={'Repeat password'} placeholder={'**********'} />
+                <form onSubmit={RegisterHandler}>
+                    <FormField 
+                        label={'Username'} 
+                        placeholder={'username'} 
+                        value={username}
+                        onChange={(e)=>setUsername(e.target.value)}
+                    />
+
+                    <FormField 
+                        label={'Password'} 
+                        placeholder={'**********'}
+                        type={'password'}
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+
+                    />
+                    <FormField 
+                        label={'Repeat password'} 
+                        placeholder={'**********'}
+                        type={'password'}
+                        value={passwordRepeat}
+                        onChange={(e)=>setPasswordRepeat(e.target.value)}
+                    />
                     <section>
                         <input required type="checkbox" />
                         <span>I have read and agree to the "Terms Of Services"</span>
