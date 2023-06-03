8129224fa7b44d7a8700f99d9d5aa6b4e2fe9d02
MichalScigalski <michalscigalski0@gmail.com>
2023-06-03 21:56:35 +0200

    saving a user in context by localstorage in useEffect

--- client/src/routes/Home/Home.component.js
+++ client/src/routes/Home/Home.component.js
@@ -1,13 +1,24 @@
 import Button from "../../components/Button/Button.component"
 import { HomeContainer } from "./Home.style"
+import { useContext, useEffect } from 'react'
+import { UserContext } from '../../context/User.Context'
+import authService from "../../services/auth.service"
 
 const Home = () => {
-    return(
+    const { setUser } = useContext(UserContext)
+
+    useEffect(() => {
+        const user = authService.getCurrentUser()
+        if (user)
+            setUser(user)
+    }, [])
+
+    return (
         <HomeContainer>
             <div>
                 <h1>Check Links!</h1>
                 <h2>Create your own custom page with social links ❤️</h2>
-                <Button $primary value={'Join now!'}/>
+                <Button $primary value={'Join now!'} />
             </div>
         </HomeContainer>
     )
