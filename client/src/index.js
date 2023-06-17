beb1551c222fe69df565f6e64590a5c04082c059
MichalScigalski <michalscigalski0@gmail.com>
2023-06-17 17:57:53 +0200

    modal binded to root

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,9 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import GlobalStyle from './globalStyles';
 import { UserContextProvider } from './context/User.context';
+import Modal from 'react-modal'
 
+Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
