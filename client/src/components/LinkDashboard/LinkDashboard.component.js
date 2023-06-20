bba520f45f06835c03faa240c9b45d421e2fca4e
MichalScigalski <michalscigalski0@gmail.com>
2023-06-20 20:49:53 +0200

    deleteLinkHandler implementation, SwitchToggle component with visibilityToggle functionality implementation

--- client/src/components/LinkDashboard/LinkDashboard.component.js
+++ client/src/components/LinkDashboard/LinkDashboard.component.js
@@ -1,11 +1,37 @@
+import { useNavigate } from 'react-router'
+import profileService from '../../services/profile.service'
 import {
     LinkDashboardContainer,
     LinkDashboardData,
     LinkDashboardButtons,
 } from './LinkDashboard.style'
+import SwitchToggle from '../SwitchToggle/SwitchToggle.component'
+import { useState } from 'react'
 
 const LinkDashboard = ({ link, openModel }) => {
-    const { name, url } = link
+    const { name, url, isVisible, _id } = link
+    const [ isChecked, setIsChecked ] = useState(isVisible)
+    const navigate = useNavigate()
+
+    const deleteLinkHandler = () => {
+        if (window.confirm(`Are you sure u want to remove ${name}?`)) {
+            try {
+                profileService.deleteLink(_id)
+                navigate(0)
+            } catch (err) {
+                alert(err.response.data.message)
+            }
+        }
+    }
+
+    const isVisibleToggleHandler = () => {
+        try {
+            profileService.toggleVisibilityLink(_id)
+            setIsChecked(!isChecked)
+        } catch (err) {
+            alert(err.response.data.message)
+        }
+    }
 
     return (
         <LinkDashboardContainer>
@@ -17,6 +43,8 @@ const LinkDashboard = ({ link, openModel }) => {
             </LinkDashboardData>
             <LinkDashboardButtons>
                 <button onClick={() => openModel(link)}>🖊️</button>
+                <button onClick={deleteLinkHandler}>🗑️</button>
+                <SwitchToggle checked={isChecked} onChange={isVisibleToggleHandler} />
             </LinkDashboardButtons>
         </LinkDashboardContainer>
     )
