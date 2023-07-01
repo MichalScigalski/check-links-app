580f19800b5a22df992643170e5ee960daed4328
MichalScigalski <michalscigalski0@gmail.com>
2023-07-01 23:59:41 +0200

    added change-password service

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import authService from './auth.service'
 
 const API_URL = process.env.REACT_APP_API_URL
 
@@ -25,10 +26,23 @@ const logout = () => {
     sessionStorage.removeItem('token')
 }
 
+const changePassword = async (newPassword) => {
+    await axios.put(
+        API_URL + '/user/change-password',
+        { newPassword },
+        {
+            headers: {
+                Authorization: 'Bearer ' + authService.getAuthToken(),
+            },
+        }
+    )
+}
+
 const userService = {
     login,
     register,
     logout,
+    changePassword
 }
 
 export default userService
