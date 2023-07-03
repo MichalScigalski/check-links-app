6bac0bdc110a1a3b65aae73e6f371e5a5d7b008c
MichalScigalski <michalscigalski0@gmail.com>
2023-07-04 01:04:42 +0200

    home rwd fixed

--- client/src/routes/Home/Home.style.js
+++ client/src/routes/Home/Home.style.js
@@ -2,18 +2,16 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
     display: flex;
-    justify-content: center;
     flex-wrap: wrap;
+    justify-content: center;
     align-items: center;
-    row-gap: 2rem;
-    height: 80vh;
-    padding-top: 2rem;
+    padding-top: 5rem;
 `
 
 export const WelcomeContainer = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+
     h1 {
         font-size: 4rem;
         span {
@@ -27,22 +25,39 @@ export const WelcomeContainer = styled.div`
         font-size: 1.75rem;
         padding: 1rem 2rem;
     }
+
+    @media (max-width: 700px) {
+        h1 {
+            font-size: 2rem;
+        }
+        button {
+            margin: 2rem 0;
+            width: max-content;
+            font-size: 1.25rem;
+            padding: 0.5rem 1rem;
+        }
+    }
 `
 
 export const CardsContainer = styled.div`
     display: flex;
-    position: relative;
+
+    @media (max-width: 700px) {
+        transform: scale(0.5);
+        margin-top: -7rem;
+    }
 
     div {
-        height: 550px;
+        max-height: 550px;
         transform: scale(0.8);
-        border-radius: 2rem;
+        border-radius: 1rem;
 
         &:nth-child(1) {
-            transform: translateX(50%);
+            transform: translateX(20%);
             z-index: 2;
         }
         &:nth-child(2) {
+            transform: translateX(-20%) scale(0.9);
             opacity: 0.6;
         }
     }
