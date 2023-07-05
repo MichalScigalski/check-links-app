ca16f0576b840d3d45a616ab0b80aa8d6e60e0ea
MichalScigalski <michalscigalski0@gmail.com>
2023-07-05 23:34:30 +0200

    added lightMode, darkMode colors configs with implement

--- client/src/globalStyles.js
+++ client/src/globalStyles.js
@@ -7,13 +7,24 @@ export const colors = {
     red: '#FE6152',
     light: '#FBFBFB',
     gray: '#EDEDED',
-    dark: '#1B2430'
+    dark: '#1B2430',
 }
 
 export const shadows = {
     small: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
     medium: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
-    large: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)'
+    large: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+}
+
+export const lightMode = {
+    body: colors.light,
+    text: '#000',
+    main: '#FFF',
+}
+export const darkMode = {
+    body: colors.dark,
+    text: '#FFF',
+    main: '#2b3a4d',
 }
 
 const GlobalStyle = createGlobalStyle`
@@ -21,7 +32,9 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         font-family: 'Outfit', sans-serif;
-        background: ${colors.light};
+        background: ${({theme}) => theme.body};
+        color: ${({theme}) => theme.text};
+        transition: background 200ms ease-in, color 200ms ease-in;
     }
 
     * {
