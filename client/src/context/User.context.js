9cf7ec6fc79b9cb994532d7f26279e1649384a99
MichalScigalski <michalscigalski0@gmail.com>
2023-07-13 21:42:10 +0200

    core.ignorecase set to false

---
+++ client/src/context/User.context.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react'
+
+export const UserContext = createContext({
+    user: null,
+    setUser: () => {},
+})
+
+export const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const value = { user, setUser }
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
