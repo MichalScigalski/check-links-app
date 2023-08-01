c32866d695cc14bf2c674896ec6f2dca6cffd0bf
MichalScigalski <michalscigalski0@gmail.com>
2023-08-01 22:25:27 +0200

    Creating show/hide password button with functionality

--- client/src/components/FormField/FormField.component.js
+++ client/src/components/FormField/FormField.component.js
@@ -1,10 +1,37 @@
-import { FormFieldContainer } from './FormField.style'
+import { FormFieldContainer, ShowPasswordButton } from './FormField.style'
+import { useState } from 'react'
+import EyeShowIcon from '../../assets/img/eye-show.svg'
+import EyeHideIcon from '../../assets/img/eye-hide.svg'
 
 const FormField = ({ label, placeholder, color, ...props }) => {
+    const [isShowPassword, setIsShowPassword] = useState(false)
+
+    const togglePasswordVisibility = () => setIsShowPassword(!isShowPassword)
+
     return (
         <FormFieldContainer color={color}>
             <label htmlFor={label}>{label}</label>
-            <input type="text" placeholder={placeholder} {...props} />
+
+            {props.type === 'password' ? (
+                <div>
+                    <input
+                        {...props}
+                        type={isShowPassword ? 'text' : 'password'}
+                        placeholder={placeholder}
+                    />
+                    <ShowPasswordButton
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                    >
+                        <img
+                            src={isShowPassword ? EyeHideIcon : EyeShowIcon}
+                            alt={isShowPassword ? 'hide-password-icon' : 'show-password-icon'}
+                        />
+                    </ShowPasswordButton>
+                </div>
+            ) : (
+                <input placeholder={placeholder} {...props} />
+            )}
         </FormFieldContainer>
     )
 }
