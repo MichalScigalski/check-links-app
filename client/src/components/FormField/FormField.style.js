666735766ca3140d194b25c1cbcc32488c23115e
MichalScigalski <michalscigalski0@gmail.com>
2023-08-01 22:26:53 +0200

    Created styled-component ShowPasswordButton

--- client/src/components/FormField/FormField.style.js
+++ client/src/components/FormField/FormField.style.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components'
-import {colors} from '../../globalStyles'
+import { colors } from '../../globalStyles'
 
 export const FormFieldContainer = styled.div`
     display: flex;
     flex-direction: column;
     label {
         margin: 5px;
+    }
+    div {
+        position: relative;
+        input {
+            padding-right: 2rem;
+        }
     }
     input {
         background-color: ${colors.gray};
@@ -21,8 +27,22 @@ export const FormFieldContainer = styled.div`
         }
         :focus {
             outline: none;
-            border-color: ${({color}) => color || colors.purple};
+            border-color: ${({ color }) => color || colors.purple};
             background-color: ${colors.light};
         }
+    }
+`
+
+export const ShowPasswordButton = styled.button`
+    position: absolute;
+    right: 10px;
+    top: 15px;
+    border: none;
+    cursor: pointer;
+    background: transparent;
+
+    img {
+        height: 15px;
+        opacity: 0.5;
     }
 `
